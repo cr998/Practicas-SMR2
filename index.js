d8ad411c06d1dcc800e242a5c86e22aea1c8bab3
cr998 <cr998m.m@gmail.com>
2018-11-14 10:41:36 +0100

    añadiendo login

--- index.js
+++ index.js
@@ -10,33 +10,45 @@ showdown.setOption('tables', 'true');
 var converter = new showdown.Converter({ extensions: ['icon'] });
 
 
+var destFolder='docs';
 
-rmdir('docs', function (err, dirs, files) {
+rmdir(destFolder, function (err, dirs, files) {
+  console.log("Borrando archivos carpeta "+destFolder)
   if(err != null && err.code != 'ENOENT'){
     console.error(err)
     process.exit(1)
   }else{
-    copy('data', 'docs', function(error, results) {
+    console.log("Borrando terminado");
+    console.log("Empezando copiado recursivo desde data");
+    copy('data', destFolder , function(error, results) {
       if (error) {
           console.error(error);
       } else {
+        console.log("copiado recursivo completado");
         walk({
           path: 'docs',
           fn: function(params, cb) {
             if(params.name.endsWith('.md')){
               fs.readFile(params.fullPath, 'utf8', (err, data) => {
                 if (err) throw err;
+                console.log("Generando html desde "+params.fullPath);
                 var nfp = params.fullPath.split("\\");
                 nfp.splice(-1,1)
                 nfp.push("index.html")
                 nfp=nfp.join("\\")
                 fs.writeFile(nfp,createHTML(converter.makeHtml(data)), function(err) {
                   if (err) throw err;
+                  console.log("guardando html en"+nfp);
                   fs.unlink(params.fullPath, (err) => {
+                    console.log("Eliminando "+params.fullPath)
                     if (err) throw err;
                   });
                 });
               });
+            }else{
+              if(!params.directory){
+                console.log("El archivo "+params.fullPath+" no cumple los requisitos")
+              }
             }
             cb()
           }
